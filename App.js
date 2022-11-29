3a026face810f293085833a51e6fdacdb70f8f99
Erica Osborn <erica.osborn@willowtreeapps.com>
2022-11-29 14:17:26 -0500

    flat list restaurant info card implemented

--- App.js
+++ App.js
@@ -1,16 +1,39 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { SearchRestaurants } from "./src/utils /SearchRestaurants";
 import { RestaurantInfoCard } from "./src/RestaurantInfoCard";
 
+const data = [
+  {
+    name: "roosters",
+    description: "good",
+  },
+  {
+    name: "house of japan",
+    description: "fancy",
+  },
+  {
+    name: "ruth chris",
+    description: "getting lucky",
+  },
+];
+
 export default function App() {
+  const renderItem = ({ item }) => (
+    <RestaurantInfoCard name={item.name} description={item.description} />
+  );
   return (
     <SafeAreaView style={styles.container}>
       <View>
         <SearchRestaurants />
       </View>
-      <View>
-        <RestaurantInfoCard />
-      </View>
+      <FlatList data={data} renderItem={renderItem} />
     </SafeAreaView>
   );
 }
@@ -18,5 +41,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "blue",
   },
 });
--- src/RestaurantInfoCard.js
+++ src/RestaurantInfoCard.js
@@ -1,27 +1,38 @@
 import React from "react";
+import { FlatList, StyleSheet, View } from "react-native";
 import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
-
 const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
 
-export const RestaurantInfoCard = () => (
-  <Card>
-    <Card.Title
-      title="Card Title"
-      subtitle="Card Subtitle"
-      left={LeftContent}
-    />
-    <Card.Content>
-      <Title>Restaurant Name</Title>
-      <Paragraph>Card content</Paragraph>
-    </Card.Content>
-    <Card.Cover
-      source={{
-        uri: "https://roosterswings.com/assets/inc/timthumb.php?w=1000&h=600&src=https://d651b0bc45559e9eaafa-f4d239947d449bf5c8da6abd73db9e1f.ssl.cf2.rackcdn.com/traditional-wings.jpg",
-      }}
-    />
-    <Card.Actions>
-      <Button>Order Now</Button>
-      <Button>leave rating</Button>
-    </Card.Actions>
-  </Card>
-);
+export const RestaurantInfoCard = (props) => {
+  const { name, description, icon, photos, address, isOpenNow, rating } = props;
+
+  return (
+    <View>
+      <Card style={styles.Card}>
+        <Card.Content>
+          <Title>{name}</Title>
+          <Paragraph>{description}</Paragraph>
+        </Card.Content>
+        <Card.Cover
+          source={{
+            uri: "https://roosterswings.com/assets/inc/timthumb.php?w=1000&h=600&src=https://d651b0bc45559e9eaafa-f4d239947d449bf5c8da6abd73db9e1f.ssl.cf2.rackcdn.com/traditional-wings.jpg",
+          }}
+        />
+        <Card.Actions>
+          <Button>Order Now</Button>
+          <Button>leave rating</Button>
+        </Card.Actions>
+      </Card>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {},
+  Card: {
+    margin: 5,
+    borderColor: "red",
+    borderWidth: 5,
+    elevation: 5,
+  },
+});
