deeb2a67655422f1ded4990e4e5239a3a5081859
Erica Osborn <erica.osborn@willowtreeapps.com>
2023-01-28 13:02:24 -0500

    Expo map view installed. Map view rendering on Map screen tab

--- src/Screens/MapScreen.js
+++ src/Screens/MapScreen.js
@@ -1,8 +1,18 @@
+import React from "react";
 import { Text, View } from "react-native";
+import MapView from "react-native-maps";
+import { StyleSheet } from "react-native";
+
 export function MapScreen() {
   return (
     <View>
-      <Text>MapScreen </Text>
+      <MapView style={styles.MapView} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  MapView: {
+    height: "100%",
+  },
+});
--- src/Screens/RestaurantDetailScreen.js
+++ src/Screens/RestaurantDetailScreen.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { List } from "react-native-paper";
 import { RestaurantInfoCard } from "../RestaurantInfoCard";
-import { MaterialCommunityIcons } from "expo-vector-icons";
+import { ScrollView } from "react-native-gesture-handler";
 
 export const RestaurantDetailScreen = ({ route }) => {
   const { item } = route.params;
@@ -19,32 +19,36 @@ export const RestaurantDetailScreen = ({ route }) => {
         isOpenNow={item.isOpenNow}
         photos={item.photos[0]}
       />
-      <List.Section style={styles.listSection} title="Menu">
-        <List.Accordion
-          title="Breakfast"
-          left={(props) => <List.Icon {...props} icon={"food-croissant"} />}
-        >
-          <List.Item title="eggs" />
-          <List.Item title="pancakes" />
-          <List.Item title="waffles" />
-        </List.Accordion>
-        <List.Accordion
-          title="Lunch"
-          left={(props) => <List.Icon {...props} icon="food-croissant" />}
-        >
-          <List.Item title="grilled cheese" />
-          <List.Item title="cheese burger" />
-          <List.Item title="quesadilla" />
-        </List.Accordion>
-        <List.Accordion
-          title="Dinner"
-          left={(props) => <List.Icon {...props} icon="food-croissant" />}
-        >
-          <List.Item title="steak" />
-          <List.Item title="fried chicken" />
-          <List.Item title="prime rib" />
-        </List.Accordion>
-      </List.Section>
+      <ScrollView>
+        <List.Section style={styles.listSection} title="Menu">
+          <List.Accordion
+            title="Breakfast"
+            left={(props) => <List.Icon {...props} icon={"food-croissant"} />}
+          >
+            <List.Item title="eggs" />
+            <List.Item title="pancakes" />
+            <List.Item title="waffles" />
+          </List.Accordion>
+
+          <List.Accordion
+            title="Lunch"
+            left={(props) => <List.Icon {...props} icon="food-croissant" />}
+          >
+            <List.Item title="grilled cheese" />
+            <List.Item title="cheese burger" />
+            <List.Item title="quesadilla" />
+          </List.Accordion>
+
+          <List.Accordion
+            title="Dinner"
+            left={(props) => <List.Icon {...props} icon="food-croissant" />}
+          >
+            <List.Item title="steak" />
+            <List.Item title="fried chicken" />
+            <List.Item title="prime rib" />
+          </List.Accordion>
+        </List.Section>
+      </ScrollView>
     </View>
   );
 };
