106d77eca069f0ed046aede80d7aa2a5be50dd04
Erica Osborn <erica.osborn@willowtreeapps.com>
2023-01-28 14:07:08 -0500

    map view link to location in mock data. zoom into region dependant on searchbar keyword

--- src/Screens/MapScreen.js
+++ src/Screens/MapScreen.js
@@ -1,14 +1,39 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import { View } from "react-native";
 import MapView from "react-native-maps";
 import { StyleSheet } from "react-native";
 import { SearchMaps } from "../utils/SearchMaps";
+import { LocationContext } from "../services/location/location.context";
+import { RestaurantsContext } from "../services/restaurants/restaurants.context";
 
 export function MapScreen() {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+  const [latDelta, setLatDelta] = useState(0);
+  const { lat, lng, viewport } = location;
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
   return (
     <View>
       <SearchMaps />
-      <MapView style={styles.MapView} />
+      <MapView
+        style={styles.MapView}
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {restaurants.map((restaurant) => {
+          return null;
+        })}
+      </MapView>
     </View>
   );
 }
