76084e8e5b438562959e712dca4ece8bc6cfa1d4
Erica Osborn <erica.osborn@willowtreeapps.com>
2023-02-06 16:49:01 -0500

    photos rendering with map marker callout

--- src/services/Maps/mapMarkerCallout.js
+++ src/services/Maps/mapMarkerCallout.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import { Platform } from "react-native";
 // import WebView from "react-native-webview";
 
 // const isAndroid = Platform.OS === "android";
 
-export const MapMarkerCallout = ({ restaurant }) => {
+export const MapMarkerCallout = ({ restaurant, photos }) => {
   return (
     <View>
       <Text>{restaurant.name}</Text>
+      <Image style={styles.Image} source={{ uri: restaurant.photos[0] }} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  Image: {
+    height: 120,
+    width: 120,
+  },
+});
