67732167478e7e3decacdd306ed336b9db074246
Erica Osborn <erica.osborn@willowtreeapps.com>
2023-02-21 15:44:18 -0500

    modal refactor WIP

--- src/Screens/RestaurantDetailModal.js
+++ src/Screens/RestaurantDetailModal.js
@@ -5,70 +5,67 @@ import {
   ScrollView,
   Button,
   TouchableWithoutFeedback,
-  TouchableOpacity,
+  Modal,
 } from "react-native";
 import { List } from "react-native-paper";
 import { RestaurantInfoCard } from "../RestaurantInfoCard";
 
 export const RestaurantDetailModal = ({ route, navigation }) => {
   const { item } = route.params;
-  const [modalShown, setModalShown] = useState(true);
+  const [modalVisible, setModalVisible] = useState(false);
 
-  if (modalShown)
-    return (
-      <TouchableWithoutFeedback
-        style={styles.restaurantDetailRender}
-        onPress={() => {
-          modalShown(false);
+  return (
+    <TouchableWithoutFeedback style={styles.restaurantDetailRender}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
         }}
       >
-        <TouchableOpacity onPressOut={() => navigation.goBack()}>
-          <Button title="Close" onPress={() => navigation.goBack()} />
-          <View>
-            <RestaurantInfoCard
-              name={item.name}
-              description={item.types}
-              rating={item.rating}
-              address={item.vicinity}
-              isOpenNow={item.isOpenNow}
-              photos={item.photos[0]}
-            />
-          </View>
-          <ScrollView>
-            <List.Section title="Menu">
-              <List.Accordion
-                title="Breakfast"
-                left={(props) => (
-                  <List.Icon {...props} icon={"food-croissant"} />
-                )}
-              >
-                <List.Item title="eggs" />
-                <List.Item title="pancakes" />
-                <List.Item title="waffles" />
-              </List.Accordion>
+        <Button title="Close" onPress={() => navigation.goBack()} />
+        <RestaurantInfoCard
+          name={item.name}
+          description={item.types}
+          rating={item.rating}
+          address={item.vicinity}
+          isOpenNow={item.isOpenNow}
+          photos={item.photos[0]}
+        />
+        <ScrollView>
+          <List.Section title="Menu">
+            <List.Accordion
+              title="Breakfast"
+              left={(props) => <List.Icon {...props} icon={"food-croissant"} />}
+            >
+              <List.Item title="eggs" />
+              <List.Item title="pancakes" />
+              <List.Item title="waffles" />
+            </List.Accordion>
 
-              <List.Accordion
-                title="Lunch"
-                left={(props) => <List.Icon {...props} icon={"food"} />}
-              >
-                <List.Item title="grilled cheese" />
-                <List.Item title="cheese burger" />
-                <List.Item title="quesadilla" />
-              </List.Accordion>
+            <List.Accordion
+              title="Lunch"
+              left={(props) => <List.Icon {...props} icon={"food"} />}
+            >
+              <List.Item title="grilled cheese" />
+              <List.Item title="cheese burger" />
+              <List.Item title="quesadilla" />
+            </List.Accordion>
 
-              <List.Accordion
-                title="Dinner"
-                left={(props) => <List.Icon {...props} icon={"food-turkey"} />}
-              >
-                <List.Item title="steak" />
-                <List.Item title="fried chicken" />
-                <List.Item title="prime rib" />
-              </List.Accordion>
-            </List.Section>
-          </ScrollView>
-        </TouchableOpacity>
-      </TouchableWithoutFeedback>
-    );
+            <List.Accordion
+              title="Dinner"
+              left={(props) => <List.Icon {...props} icon={"food-turkey"} />}
+            >
+              <List.Item title="steak" />
+              <List.Item title="fried chicken" />
+              <List.Item title="prime rib" />
+            </List.Accordion>
+          </List.Section>
+        </ScrollView>
+      </Modal>
+    </TouchableWithoutFeedback>
+  );
 };
 const styles = StyleSheet.create({
   restaurantDetailRender: {
